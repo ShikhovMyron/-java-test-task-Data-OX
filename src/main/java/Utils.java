2af7d5cf30d241751d4027eb9a66d24b7da7eb1d
Myron <shikhovmyron@gmail.com>
2022-04-07 20:21:22 +0200

    add static methods for to fill the building with passengers

--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -1,9 +1,17 @@
+import java.util.Random;
+
 public class Utils {
 
     public static void fillBuildingByPassengers(Building building, int minOnFloor, int maxOnFloor){
         for (int i = 0; i < building.getFloorsCount(); i++) {
-
+            for (int j = 0; j < getRandomNumber(minOnFloor, maxOnFloor); j++) {
+                building.putPassenger(i,new Passenger("",getRandomNumber(0,building.getFloorsCount())));
+            }
         }
+    }
+
+    private static int getRandomNumber(int min, int max) {
+        return new Random().nextInt(max+1-min)+min;
     }
 
 
